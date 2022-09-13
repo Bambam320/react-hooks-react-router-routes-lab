ec052de1558a26590e7ee61b9e99cc39e27ade3b
Igor Mestechkin <Igor.mestechkin@gmail.com>
2022-09-13 16:05:50 -0400

    Completed Assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,27 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const listMovies = movies.map((movie) => {
+    return (
+      <div key={movie.title}>
+        <h1>{movie.title}</h1>
+        <h2>{`Runtime: ${movie.time} minutes`}</h2>
+        <ul>
+          {movie.genres.map((genre) => {
+            return <li key={genre}>{genre}</li>
+          })}
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {listMovies}
+    </div>
+  );
 }
 
 export default Movies;
